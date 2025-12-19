6c4a70e15ad03fed528500fbaff5be69f9ed5964
Yejin1 <dpwls273273@gmail.com>
2025-12-19 18:41:02 +0900

    백준 12869 뮤탈리스크

--- src/main/java/boj/_12869_mutalisk.java
+++ src/main/java/boj/_12869_mutalisk.java
@@ -1,22 +1,77 @@
 package boj;
 
+// 백준
+// 12869 뮤탈리스크
+// 유형 : bfs
+// https://www.acmicpc.net/problem/12869
+
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
+import static java.lang.Math.max;
+
 public class _12869_mutalisk {
+
+    public static class state {
+        int x, y, z;
+
+        public state(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    static int[][] damage =
+            {
+                    {1,3,9},
+                    {1,9,3},
+                    {3,1,9},
+                    {3,9,1},
+                    {9,1,3},
+                    {9,3,1}
+            };
+
     public static void main(String[] args) {
         //첫째 줄에 SCV의 수 N (1 ≤ N ≤ 3)이 주어진다. 둘째 줄에는 SCV N개의 체력이 주어진다. 체력은 60보다 작거나 같은 자연수이다.;
-
-        //!!!!!!!!! 아직 안풀었음!!!!!!!!!!!!!!!!!!!!!!!
+        //첫째 줄에 모든 SCV를 파괴하기 위한 공격 횟수의 최솟값을 출력한다.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] scv = new int[n];
+        int[] scv = new int[3];
 
         for (int i = 0; i < n; i++) {
             scv[i] = sc.nextInt();
         }
 
-        int[][] d = new int[60][n];
-        int count = 0;
+        int[][][] d = new int[61][61][61];
+        for (int i = 0; i < 61; i++) {
+            for (int j = 0; j < 61; j++) {
+                for (int k = 0; k < 61; k++) {
+                    d[i][j][k] = -1;
+                }
+            }
+        }
+        Queue<state> q = new LinkedList<>();
+        q.add(new state(scv[0], scv[1], scv[2]));
+        d[scv[0]][scv[1]][scv[2]] = 0;
+
+        while (!q.isEmpty()) {
+            state cur = q.poll();
+            for (int i = 0; i < 6; i++) {
+                int x = max(cur.x - damage[i][0],0);
+                int y = max(cur.y - damage[i][1],0);
+                int z = max(cur.z - damage[i][2],0);
+
+                if (d[x][y][z] == -1 || d[x][y][z] > d[cur.x][cur.y][cur.z]+1) {
+                    d[x][y][z] = d[cur.x][cur.y][cur.z] + 1;
+                    q.add(new state(x,y,z));
+                }
+            }
+        }
+
+        System.out.println(d[0][0][0]);
+
 
     }
 }
---
+++ src/main/java/boj/_1765_unheard.java
@@ -0,0 +1,44 @@
+package boj;
+// 백준
+// 1765 듣보잡
+// 유형 : 자료구조 활용
+// https://www.acmicpc.net/problem/1765
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class _1765_unheard {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] nm = bf.readLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+
+        Set<String> names = new HashSet<>();
+        ArrayList<String> unheard = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            names.add(bf.readLine());
+        }
+
+        for (int i = 0; i < m; i++) {
+            String tmp = bf.readLine();
+            if (names.contains(tmp)) {
+                unheard.add(tmp);
+            }
+        }
+
+        Collections.sort(unheard);
+
+        System.out.println(unheard.size());
+        for (int i = 0; i < unheard.size(); i++) {
+            System.out.println(unheard.get(i));
+        }
+    }
+}
+
